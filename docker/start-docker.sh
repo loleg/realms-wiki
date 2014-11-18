131e549fd729a173717a99a5acc9bf1f464e7670
Oleg Lavrovsky <oleg@utou.ch>
2014-11-18 11:50:30 +0000

    Postgres script

---
+++ docker/start-docker.sh
@@ -0,0 +1,9 @@
+sudo mkdir -p /srv/realms/etc /srv/realms/wiki
+sudo chmod 777 /srv/realms/wiki
+sudo cp realms-wiki.json /srv/realms/etc
+sudo docker run --name realms-wiki -p 5000:5000 -d \
+ --link postgresql:db -v /srv/realms/wiki:/opt/wiki -v /srv/realms/etc:/etc/realms-wiki/ \
+ realms-wiki-git
+
+echo "sudo docker exec -it realms-wiki /usr/bin/tail -f /var/log/realms-wiki/realms-wiki.log"
+# ./lib/python2.7/site-packages/realms/static/css/style.css
