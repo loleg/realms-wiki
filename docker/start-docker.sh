95dbf3a4bf3e7421a13763f5733b0a4ade79656e
Oleg Lavrovsky <oleg@utou.ch>
2014-11-18 12:04:03 +0000

    Static deploy test

--- docker/start-docker.sh
+++ docker/start-docker.sh
@@ -1,9 +1,12 @@
-sudo mkdir -p /srv/realms/etc /srv/realms/wiki
+sudo mkdir -p /srv/realms/etc /srv/realms/wiki 
 sudo chmod 777 /srv/realms/wiki
+
 sudo cp realms-wiki.json /srv/realms/etc
-sudo docker run --name realms-wiki -p 5000:5000 -d \
- --link postgresql:db -v /srv/realms/wiki:/opt/wiki -v /srv/realms/etc:/etc/realms-wiki/ \
+
+sudo docker run --name realms-wiki -p 5000:5000 -d --link postgresql:db \
+ -v /srv/realms/wiki:/opt/wiki -v /srv/realms/etc:/etc/realms-wiki/ \
  realms-wiki-git
 
+#sudo docker exec -it realms-wiki cp -r /home/deploy/realms-wiki/.venv/lib/python2.7/site-packages/realms/static /tmp/static
+
 echo "sudo docker exec -it realms-wiki /usr/bin/tail -f /var/log/realms-wiki/realms-wiki.log"
-# ./lib/python2.7/site-packages/realms/static/css/style.css
